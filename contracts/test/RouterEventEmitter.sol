a12e87ae9d2cad7b5a73c723ede144beb8a5d364
BuildTools <roderick_lu@163.com>
2021-06-01 20:08:36 +0800

    init

---
+++ contracts/test/RouterEventEmitter.sol
@@ -0,0 +1,97 @@
+pragma solidity =0.6.6;
+
+import '../interfaces/IUniswapV2Router01.sol';
+
+contract RouterEventEmitter {
+    event Amounts(uint[] amounts);
+
+    receive() external payable {}
+
+    function swapExactTokensForTokens(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapExactTokensForTokens.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactTokens(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapTokensForExactTokens.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactAVAXForTokens(
+        address router,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapExactAVAXForTokens.selector, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapTokensForExactAVAX(
+        address router,
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapTokensForExactAVAX.selector, amountOut, amountInMax, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapExactTokensForAVAX(
+        address router,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapExactTokensForAVAX.selector, amountIn, amountOutMin, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+
+    function swapAVAXForExactTokens(
+        address router,
+        uint amountOut,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable {
+        (bool success, bytes memory returnData) = router.delegatecall(abi.encodeWithSelector(
+            IUniswapV2Router01(router).swapAVAXForExactTokens.selector, amountOut, path, to, deadline
+        ));
+        assert(success);
+        emit Amounts(abi.decode(returnData, (uint[])));
+    }
+}
